b322c9ea1039b77d618eaec7dbe582fb4aec4c50
Vadym Marukhnenko <vdm.marukhnenko@gmail.com>
2023-09-07 15:26:34 +0100

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [CommonModule],
 })
-export class AppComponent {
-  title = 'todo-app';
-}
+export class AppComponent {}
